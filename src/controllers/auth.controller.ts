e437bf92f0b9f3864e5bf5af5684291393bc3ba2
Nguyen Quoc Dai <daihoc2k20@gmail.com>
2023-05-07 20:03:04 +0700

    add atlas

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,7 +5,7 @@ import { Response } from 'express';
 const token =
   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-@Controller('auth')
+@Controller('api')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
